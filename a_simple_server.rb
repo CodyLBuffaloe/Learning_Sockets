5c1d730cd6f6b23dbaf4fa1f632b333aee426ac4
Cody Buffaloe <codylbuffaloe@gmail.com>
2016-12-22 18:05:13 -0800

    Fixed reg ex to actually capture GET request

--- a_simple_server.rb
+++ a_simple_server.rb
@@ -4,10 +4,10 @@ server = TCPServer.open(80)
 loop{
   client = server.accept
   puts "Contact!"
-  if(/([\w]+)/ =~ "GET")
-    puts "It worked!"
-  end
   while line = client.gets
+    if(line =~ /GET/)
+      puts "It worked!"
+    end
     puts line.chop
   end
   client.close
