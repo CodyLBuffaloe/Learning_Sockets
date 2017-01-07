f0929381dc388db50914aa0ac49a0119f31900e2
Cody Buffaloe <codylbuffaloe@gmail.com>
2017-01-06 16:53:12 -0800

    Returns OK response message when index.html is pinged

--- a_simple_server.rb
+++ a_simple_server.rb
@@ -3,11 +3,11 @@ require 'socket'
 server = TCPServer.open(80)
 loop{
   client = server.accept
-  puts "Contact!"
   while line = client.gets
     if(line =~ /GET/)
       if(line =~ /index/)
-        puts "It worked!"
+        puts "HTTP/1.1 200 OK"
+
       else
         puts "ERROR 404: THE RESOURCE YOU HAVE REQUESTED DOES NOT EXIST"
       end
