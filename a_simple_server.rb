22df73136377de562a1f8b1dd6e929a78d9caaba
Cody Buffaloe <codylbuffaloe@gmail.com>
2017-01-10 16:44:00 -0800

    Prints a string to the screen...after you manually close the connection.

--- a_simple_server.rb
+++ a_simple_server.rb
@@ -2,11 +2,18 @@ require 'socket'
 
 server = TCPServer.open(80)
 loop{
-  client = server.accept
-  while line = client.gets
+  socket = server.accept
+  while line = socket.gets
     if(line =~ /GET/)
       if(line =~ /index/)
-        puts "HTTP/1.1 200 OK"
+        response = "/index.html"
+        socket.print response
+
+        puts "HTTP/1.1 200 OK\r\n" +
+             "Content-Type: text/plain\r\n" +
+             "Content-Length: an amount\r\n" +
+             "Connection: close\r\n"
+
 
       else
         puts "ERROR 404: THE RESOURCE YOU HAVE REQUESTED DOES NOT EXIST"
@@ -14,5 +21,5 @@ loop{
     end
     puts line.chop
   end
-  client.close
+  socket.close
 }
