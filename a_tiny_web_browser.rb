232f3c805eb82eb130d53c8e5008354b67253747
Cody Buffaloe <codylbuffaloe@gmail.com>
2017-01-17 13:48:40 -0800

    Created a new html page for project.

--- a_tiny_web_browser.rb
+++ a_tiny_web_browser.rb
@@ -2,14 +2,14 @@
 
 require 'socket'
 
-host = 'www.neopets.com'
+host = 'localhost'
 port = 80
-path = "/index.phtml"
+path = "/index.html"
 
 request = "GET #{path} HTTP/1.0\r\n\r\n"
 
 socket = TCPSocket.open(host, port)
 socket.print(request)
 response = socket.read
-headers, body = response.split("\r\n\r\n", 2)
+body = response.split("\r\n", 2)
 print body
