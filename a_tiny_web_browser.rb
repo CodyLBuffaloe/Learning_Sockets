73b88ac97fd45e9528183509b70e1dc0b620d0ee
Cody Buffaloe <codylbuffaloe@gmail.com>
2017-01-17 17:26:06 -0800

    Separated the different actions into if/elsif

--- a_tiny_web_browser.rb
+++ a_tiny_web_browser.rb
@@ -5,15 +5,23 @@ require 'socket'
 puts "Would you like to get a page or post something? Type 'g' to GET a page " +
      "and 'p' to POST something."
 user_request = gets.chomp
+if(user_request = "g")
+  host = 'localhost'
+  port = 80
+  path = "/index.html"
 
-host = 'localhost'
-port = 80
-path = "/index.html"
+  request = "GET #{path} HTTP/1.0\r\n\r\n"
 
-request = "GET #{path} HTTP/1.0\r\n\r\n"
+  socket = TCPSocket.open(host, port)
+  socket.print(request)
+  response = socket.read
+  body = response.split("\r\n", 2)
+  print body
+elsif(user_request = "p")
+  puts "You're signing up for a Viking raid! Please provide your name:"
+  name = gets.chomp
+  puts "And your e-mail address:"
+  e-mail = gets.chomp
+  
 
-socket = TCPSocket.open(host, port)
-socket.print(request)
-response = socket.read
-body = response.split("\r\n", 2)
-print body
+end
